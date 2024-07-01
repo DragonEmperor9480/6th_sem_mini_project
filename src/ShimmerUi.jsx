6758208820e7486f57a576f45c9f455240c35cde
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-07-01 13:53:35 +0530

    Implement ShimmerUI
    * a temporary loading screen which shows before the data is fetched

---
+++ src/ShimmerUi.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+const ShimmerUI = () => {
+    return(
+        <div className="h-[90vh] w-screen flex flex-wrap justify-around">
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+            <div className="skeleton w-96 h-96 m-4"></div>
+
+        </div>
+    )
+}
+
+export default ShimmerUI;
