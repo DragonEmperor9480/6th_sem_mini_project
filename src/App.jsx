97943071ea07f5b22ec8a61e6aabe8aef2196cd5
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-07-01 13:55:31 +0530

    Update App.jsx with ThemeContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import './index.css'
+import { useState } from 'react'
+import './App.css'
+import Home from './Home'
 import Navbar from './Navbar'
+import { Outlet } from 'react-router-dom'
+import ThemeContext from './utility/ThemeContext'
 
 function App() {
 
   return (
     <>
+    <ThemeContext>
     <Navbar></Navbar>
+    <Outlet></Outlet>
+    </ThemeContext>
     </>
   )
 }
