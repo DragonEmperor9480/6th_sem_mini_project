d5b49b4866dd3415a6891c079fe53e842460cfa2
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2024-07-23 11:20:14 +0530

    App.jsx: drop unecessary imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useState } from 'react'
-import Home from './Home'
 import Navbar from './Navbar'
 import { Outlet } from 'react-router-dom'
 import ThemeContext from './utility/ThemeContext'
